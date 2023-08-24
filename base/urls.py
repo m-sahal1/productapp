4262bf92943e9c39d42373eea93316b66507a789
m-sahal1 <sahal9212500727@gmail.com>
2023-08-24 10:47:56 +0530

    Added Views

--- base/urls.py
+++ base/urls.py
@@ -18,8 +18,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from . import views
 
 urlpatterns = [
+    path('products/', views.list_products, name='products'),
+    path('variants/', views.list_variants, name='variants'),
+    path('collections/', views.list_collections, name='collections'),
+    path('collections/<str:pk>/products', views.list_collection_products, name='collection-of-products'),
+    path('collections/<str:pk>/variations', views.list_collection_variations, name='collection-of-variation'),
+
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- base/views.py
+++ base/views.py
@@ -1,3 +1,50 @@
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
+from .models import Category, Collection, Product, Variant, Image
 
-# Create your views here.
+
+def list_products(request):
+    products = Product.objects.prefetch_related('variants__image').all()
+    # Shorten it with values method
+    products_list = []
+    for product in products:
+        product_dict = {
+            'title': product.title,
+            'description': product.description,
+            'created_at': product.created_at,
+            'updated_at': product.updated_at,
+            'images': [variant.image.source.url for variant in product.variants.all()]
+        }
+        products_list.append(product_dict)
+    
+    return JsonResponse(products_list, safe=False)
+
+
+def list_variants(request):
+    variants= Variant.objects.select_related('image').all()
+    variants_dict_queryset=variants.values('title','created_at','updated_at', 'available_for_sale', 'price','image__source')
+    variants_list=list(variants_dict_queryset)
+    return JsonResponse(variants_list, safe=False)
+
+
+def list_collections(request):
+    collections= Collection.objects.all().values('title','published', 'updated_at')
+    return JsonResponse(list(collections), safe=False)
+
+
+def list_collection_products(request, pk):
+    collection= Collection.objects.prefetch_related('products','products__variants__image').get(id=pk)
+    products= collection.products.all().values('title', 'description', 'created_at', 'updated_at', 'variants__image__source')
+    return JsonResponse(list(products), safe=False)
+
+
+def list_collection_variations(request, pk):
+    collection= Collection.objects.prefetch_related('products__variants','products__variants__image').get(id=pk)
+    variants = Variant.objects.filter(product__in=collection.products.all()).values(
+        'title', 'created_at', 'updated_at', 'available_for_sale', 'price', 'image__source'
+    )
+    return JsonResponse(list(variants), safe=False)
+
+
+
+
