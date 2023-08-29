3516693f45e0a0f99c0566e68335727dc58b3f4e
m-sahal1 <sahal9212500727@gmail.com>
2023-08-29 16:42:57 +0530

    Added DRF APIs, Serializers

--- base/api/serializers.py
+++ base/api/serializers.py
@@ -1,6 +1,32 @@
 from rest_framework import serializers
-from base.models import Category, Collection, Product, Variant
+from base.models import Category, Collection, Product, Variant, Image
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ['source', 'alt_text']
+
+class VariantSerializer(serializers.ModelSerializer):
+    image = ImageSerializer()
+    class Meta:
+        model = Variant
+        fields = ['title','created_at','updated_at', 'available_for_sale', 'price','image']
+
+class ProductSerializer(serializers.ModelSerializer):
+    variants = VariantSerializer(many=True)
+    
+    class Meta:
+        model = Product
+        fields = ['title', 'description', 'created_at', 'updated_at', 'variants']
+
 class CategorySerializer(serializers.ModelSerializer):
-    class META:
+    class Meta:
         model = Category
         fields = '__all__'
+
+
+class CollectionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Collection
+        fields = ['title','published', 'updated_at']
--- base/api/views.py
+++ base/api/views.py
@@ -1,49 +1,45 @@
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from base.models import Category, Collection, Product, Variant, Image
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import ProductSerializer, VariantSerializer, CollectionSerializer
 
-
+@api_view(['GET'])
 def list_products(request):
-    products = Product.objects.prefetch_related('variants__image').all()
-    # Shorten it with values method
-    products_list = []
-    for product in products:
-        product_dict = {
-            'title': product.title,
-            'description': product.description,
-            'created_at': product.created_at,
-            'updated_at': product.updated_at,
-            'images': [variant.image.source.url for variant in product.variants.all()]
-        }
-        products_list.append(product_dict)
-    
-    return JsonResponse(products_list, safe=False)
+    products = Product.objects.prefetch_related('variants','variants__image').all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 
+@api_view(['GET'])
 def list_variants(request):
     variants= Variant.objects.select_related('image').all()
-    variants_dict_queryset=variants.values('title','created_at','updated_at', 'available_for_sale', 'price','image__source')
-    variants_list=list(variants_dict_queryset)
-    return JsonResponse(variants_list, safe=False)
+    serializer = VariantSerializer(variants, many= True)
+    return Response(serializer.data)
 
 
+@api_view(['GET'])
 def list_collections(request):
-    collections= Collection.objects.all().values('title','published', 'updated_at')
-    return JsonResponse(list(collections), safe=False)
+    collections= Collection.objects.all()
+    serializer = CollectionSerializer(collections, many = True)
+    return Response(serializer.data)
 
 
+@api_view(['GET'])
 def list_collection_products(request, pk):
     collection= Collection.objects.prefetch_related('products','products__variants__image').get(id=pk)
     products= collection.products.all().values('title', 'description', 'created_at', 'updated_at', 'variants__image__source')
-    return JsonResponse(list(products), safe=False)
+    return Response(products)
 
 
+@api_view(['GET'])
 def list_collection_variations(request, pk):
     collection= Collection.objects.prefetch_related('products__variants','products__variants__image').get(id=pk)
     variants = Variant.objects.filter(product__in=collection.products.all()).values(
         'title', 'created_at', 'updated_at', 'available_for_sale', 'price', 'image__source'
     )
-    return JsonResponse(list(variants), safe=False)
+    return Response(variants)
 
 
 def get_category_ids(cat_id):
@@ -58,11 +54,11 @@ def get_category_ids(cat_id):
 
     return cat_ids
     
-
+@api_view(['GET'])
 def list_category_variations(request,pk):
     category_ids = get_category_ids(pk)
     products= Product.objects.filter(category__in = category_ids).prefetch_related('variants','variants__image')
     variants= Variant.objects.filter(product__in = products).values(
         'title', 'created_at', 'updated_at', 'available_for_sale', 'price', 'image__source'
         )
-    return JsonResponse(list(variants), safe=False)
+    return Response(variants)
